2b78e6506ae09126452900a66ab592cad6f83153
Jorge Santos <jorge.m.santos@tecnico.ulisboa.pt>
2023-11-27 18:17:02 +0000

    Added states

--- dataset_1/2-data_preparation/code/encoding.py
+++ dataset_1/2-data_preparation/code/encoding.py
@@ -9,19 +9,13 @@ yes_no: dict[str, int] = {"no": 0, "No": 0, "yes": 1, "Yes": 1}
 sex_values: dict[str, int] = {"Male": 0, "Female": 1}
 
 state_values: dict[str, int] = {
-    #dummify???
-    '''
-    'Alabama' 'Alaska' 'Arizona' 'Arkansas' 'California' 'Colorado'
-    'Connecticut' 'Delaware' 'District of Columbia' 'Florida' 'Georgia'
-    'Hawaii' 'Idaho' 'Illinois' 'Indiana' 'Iowa' 'Kansas' 'Kentucky'
-    'Louisiana' 'Maine' 'Maryland' 'Massachusetts' 'Michigan' 'Minnesota'
-    'Mississippi' 'Missouri' 'Montana' 'Nebraska' 'Nevada' 'New Hampshire'
-    'New Jersey' 'New Mexico' 'New York' 'North Carolina' 'North Dakota'
-    'Ohio' 'Oklahoma' 'Oregon' 'Pennsylvania' 'Rhode Island' 'South Carolina'
-    'South Dakota' 'Tennessee' 'Texas' 'Utah' 'Vermont' 'Virginia'
-    'Washington' 'West Virginia' 'Wisconsin' 'Wyoming' 'Guam' 'Puerto Rico'
-    'Virgin Islands'
-    '''
+    'Alabama': 0, 'Alaska' : 1, 'Arizona': 2, 'Arkansas': 3, 'California': 4, 'Colorado': 5, 'Connecticut': 6, 'Delaware': 7, 
+    'District of Columbia': 8, 'Florida': 9, 'Georgia': 10, 'Hawaii': 11, 'Idaho': 12, 'Illinois': 13, 'Indiana': 14, 'Iowa': 15, 
+    'Kansas': 16, 'Kentucky': 17, 'Louisiana': 18, 'Maine': 19, 'Maryland': 20, 'Massachusetts': 21, 'Michigan': 22, 'Minnesota': 23,
+    'Mississippi': 24, 'Missouri': 25, 'Montana': 26, 'Nebraska': 27, 'Nevada': 28, 'New Hampshire': 29, 'New Jersey': 30, 'New Mexico': 31, 
+    'New York': 32, 'North Carolina': 33, 'North Dakota': 34, 'Ohio': 35, 'Oklahoma': 36, 'Oregon': 37, 'Pennsylvania': 38, 'Rhode Island': 39, 
+    'South Carolina': 40, 'South Dakota': 41, 'Tennessee': 42, 'Texas': 43, 'Utah': 44, 'Vermont': 45, 'Virginia': 46, 'Washington': 47, 
+    'West Virginia': 48, 'Wisconsin': 49, 'Wyoming': 50, 'Guam': 51, 'Puerto Rico': 52, 'Virgin Islands': 53
 }
 
 health_values: dict[str, int] = {
