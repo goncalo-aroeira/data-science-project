9fc48d66438c41688eeac2117d7226bd7983db6d
José Cruz <jose.melo.neves.cruz@tecnico.ulisboa.pt>
2023-11-28 00:06:04 +0000

    feat: add outliers treatment with eval and decision

--- dataset2/lab2.py
+++ dataset2/lab2.py
@@ -1,6 +1,6 @@
-from pandas import read_csv, DataFrame
+from pandas import read_csv, DataFrame, Series
 from dslabs_functions import (get_variable_types, mvi_by_filling, evaluate_approach, 
-                              plot_multibar_chart, determine_outlier_thresholds_for_var, NR_STDEV)
+                              plot_multibar_chart, determine_outlier_thresholds_for_var)
 from math import pi, sin, cos
 from numpy import nan
 from matplotlib.pyplot import show, savefig, figure, close
@@ -179,7 +179,7 @@ def encoding(data: DataFrame, file_tag: str):
     data.replace(encoding, inplace=True)
 
     print(f"the data shape after the encoding: {data.shape}")
-    data.to_csv("data/ccs_vars_encoded.csv")
+    data.to_csv("data/ccs_vars_encoded.csv", index=False)
 
     print(data.head(20))
 
@@ -211,21 +211,21 @@ def missing_values_imputation(data_filename: str, og_symb_vars: list[str], og_nu
     data.dropna(axis=0, thresh=round(data.shape[1] * MIN_MV_IN_A_RECORD_RATIO, 0), inplace=True)
 
     data_filling_frequent = mvi_by_filling(data, "frequent", og_symb_vars + unique_loans, og_num_vars)
-    data_filling_frequent.to_csv("data/ccs_mvi_fill_frequent.csv")
+    data_filling_frequent.to_csv("data/ccs_mvi_fill_frequent.csv", index=False)
     data_filling_knn = mvi_by_filling(data, "knn", og_symb_vars, og_num_vars, 3)
-    data_filling_knn.to_csv("data/ccs_mvi_fill_knn.csv")
+    data_filling_knn.to_csv("data/ccs_mvi_fill_knn.csv", index=False)
 
 def mvi_evaluation(target: str, file_tag: str):
     frequent_fn = "data/ccs_mvi_fill_frequent.csv"
     knn_fn = "data/ccs_mvi_fill_knn.csv"
     data_frequent_mvi_fill: DataFrame = read_csv(frequent_fn)
-    data_knn_mvi_fill: DataFrame = read_csv(knn_fn, index_col="ID")
+    data_knn_mvi_fill: DataFrame = read_csv(knn_fn, index_col="ID") ## WHY INDEX_COL?????
 
     figure()
     eval: dict[str, list] = evaluate_approach(data_frequent_mvi_fill.head(int(data_frequent_mvi_fill.shape[0]*0.8)), 
                                               data_frequent_mvi_fill.tail(int(data_frequent_mvi_fill.shape[0]*0.2)), 
                                               target=target, metric="recall")
-    plot_multibar_chart(["NB", "KNN"], eval, title=f"{file_tag} evaluation", percentage=True)
+    plot_multibar_chart(["NB", "KNN"], eval, title="MVI Frequence Strat evaluation", percentage=True)
     savefig(f"images/{file_tag}_mvi_freq_eval.png")
     show()
     close()
@@ -234,7 +234,7 @@ def mvi_evaluation(target: str, file_tag: str):
     eval: dict[str, list] = evaluate_approach(data_knn_mvi_fill.head(int(data_knn_mvi_fill.shape[0]*0.8)), 
                                               data_knn_mvi_fill.tail(int(data_knn_mvi_fill.shape[0]*0.2)), 
                                               target=target, metric="recall")
-    plot_multibar_chart(["NB", "KNN"], eval, title=f"{file_tag} evaluation", percentage=True)
+    plot_multibar_chart(["NB", "KNN"], eval, title="MVI KNN Strat evaluation", percentage=True)
     savefig(f"images/{file_tag}_mvi_knn_eval.png")
     show()
 
@@ -247,6 +247,60 @@ def mvi_evaluation(target: str, file_tag: str):
 
 def outliers_treatment(data_filename: str):
     data: DataFrame = read_csv(data_filename)
+
+    # Approach 1 - Dropping outliers (std_based = true)
+    print(f"Data before dropping outliers: {data.shape}")
+    data_outliers_rowDrop: DataFrame = data.copy(deep=True)
+    summary5: DataFrame = data.describe()
+    for var in data.columns:
+        top_threshold, bottom_threshold = determine_outlier_thresholds_for_var(summary5[var])
+        outliers: Series = data_outliers_rowDrop[(data_outliers_rowDrop[var] > top_threshold) | (data_outliers_rowDrop[var] < bottom_threshold)]
+        data_outliers_rowDrop.drop(outliers.index, axis=0, inplace=True)
+    data_outliers_rowDrop.to_csv("data/ccs_outliers_rowDrop_stdBased.csv", index=False)
+    print(f"Data after dropping outliers: {data_outliers_rowDrop.shape}")
+
+    # Approach 2 - Dropping outliers (std_based = false)
+    print(f"Data before dropping outliers: {data.shape}")
+    data_outliers_rowDrop: DataFrame = data.copy(deep=True)
+    summary5: DataFrame = data.describe()
+    for var in data.columns:
+        top_threshold, bottom_threshold = determine_outlier_thresholds_for_var(summary5[var], std_based=False)
+        outliers: Series = data_outliers_rowDrop[(data_outliers_rowDrop[var] > top_threshold) | (data_outliers_rowDrop[var] < bottom_threshold)]
+        data_outliers_rowDrop.drop(outliers.index, axis=0, inplace=True)
+    data_outliers_rowDrop.to_csv("data/ccs_outliers_rowDrop_NotStdBased.csv", index=False)
+    print(f"Data after dropping outliers: {data_outliers_rowDrop.shape}")
+
+    # Approach 3 - Replacing Outliers (if > than max or < min) with fixed values - using the median
+    data_outliers_rep_fixedMedian: DataFrame = data.copy(deep=True)
+    for var in data.columns:
+        top, bottom = determine_outlier_thresholds_for_var(summary5[var])
+        median: float = data_outliers_rep_fixedMedian[var].median()
+        data_outliers_rep_fixedMedian[var] = data_outliers_rep_fixedMedian[var].apply(lambda x: median if x > top or x < bottom else x)
+    data_outliers_rep_fixedMedian.to_csv("data/ccs_outliers_rep_fixed_median.csv", index=True)
+
+    # Approach 4 - Truncating outliers with the max and min
+    data_outliers_trunc: DataFrame = data.copy(deep=True)
+    for var in data.columns:
+        top, bottom = determine_outlier_thresholds_for_var(summary5[var])
+        data_outliers_trunc[var] = data_outliers_trunc[var].apply(lambda x: top if x > top else bottom if x < bottom else x)
+    data_outliers_trunc.to_csv("data/ccs_outliers_trunc_minmax.csv", index=True)
+
+    # Evaluate the different approaches
+    outliers_evaluation("data/ccs_outliers_rowDrop_stdBased.csv", "rowDrop_stdBased")
+    outliers_evaluation("data/ccs_outliers_rowDrop_NotStdBased.csv", "rowDrop_NotStdBased")
+    outliers_evaluation("data/ccs_outliers_rep_fixed_median.csv", "rep_fixed_median")
+    outliers_evaluation("data/ccs_outliers_trunc_minmax.csv", "truncating_minmax")
+
+def outliers_evaluation(data_filename: str, strategy: str):
+    data: DataFrame = read_csv(data_filename)
+
+    figure()
+    eval: dict[str, list] = evaluate_approach(data.head(int(data.shape[0]*0.8)),
+                                              data.tail(int(data.shape[0]*0.2)),
+                                              target=target, metric="recall")
+    plot_multibar_chart(["NB", "KNN"], eval, title=f"Outliers Treatment with {strategy} evaluation", percentage=True)
+    savefig(f"images/Credit_Score_outliers_treat_{strategy}.png")
+    show()
 
 #***************************************************************************************************
 
@@ -270,13 +324,16 @@ if __name__ == "__main__":
     og_unique_loans = get_all_unique_loans(data)
 
     # variable encoding step
-    encoding(data, file_tag)
-    newNoMissing(data, file_tag)
+    #encoding(data, file_tag)
+    #newNoMissing(data, file_tag)
 
     # missing values imputation step
-    missing_values_imputation("data/ccs_vars_encoded.csv", og_symb_vars, og_num_vars, og_unique_loans)
-    mvi_evaluation(target, file_tag)
+    #missing_values_imputation("data/ccs_vars_encoded.csv", og_symb_vars, og_num_vars, og_unique_loans)
+    #mvi_evaluation(target, file_tag)
 
     # outliers treatment
     mvi_decided_data = "data/ccs_mvi_fill_knn.csv"
-    #outliers_treatment(mvi_decided_data)    
+    outliers_treatment(mvi_decided_data)
+
+    # Scaling
+    outliers_treat_decision = "data/ccs_outliers_rowDrop_stdBased.csv"
