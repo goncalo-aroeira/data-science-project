0ee3c1d8b756860ed2180043c95a9c1181c918f6
Gonçalo Aroeira Gonçalves <goncalo.b.aroeira@tecnico.ulisboa.pt>
2023-11-30 13:15:15 +0000

    done outliers but still errors in scaling

--- dataset_1/2-data_preparation/code/outliers.py
+++ dataset_1/2-data_preparation/code/outliers.py
@@ -1,11 +1,29 @@
 from pandas import read_csv, DataFrame, Series
 from matplotlib.pyplot import figure, savefig, close, show
 from dslabs_functions import determine_outlier_thresholds_for_var, plot_multibar_chart, evaluate_approach
+from sklearn.model_selection import train_test_split
 
+def outliers_evaluation(data_filename: str, strategy: str):
+    data: DataFrame = read_csv(data_filename)
+
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+    ## Evaluate Approach
+    file_tag = "CovidPos"
+    target = "CovidPos"
+
+    figure()
+    eval: dict[str, list] = evaluate_approach(train_data, test_data, target=target, metric="recall")
+    plot_multibar_chart(
+        ["NB", "KNN"], eval, title=f"{file_tag} evaluation", percentage=True)
+
+    savefig(f"../images/CovidPos_outliers_treat_{strategy}.png")
+    show()
+    
+    
 target = "CovidPos"
 
 
-data: DataFrame = read_csv("../data/ccs_mvi_fill_frequent_shuffle.csv")
+data: DataFrame = read_csv("../data/ccs_mvi_fill_frequent.csv")
 # Approach 1 - Dropping outliers (std_based = true)
 print(f"Data before dropping outliers: {data.shape}")
 data_outliers_rowDrop: DataFrame = data.copy(deep=True)
@@ -14,8 +32,9 @@ for var in data.columns:
     top_threshold, bottom_threshold = determine_outlier_thresholds_for_var(summary5[var])
     outliers: Series = data_outliers_rowDrop[(data_outliers_rowDrop[var] > top_threshold) | (data_outliers_rowDrop[var] < bottom_threshold)]
     data_outliers_rowDrop.drop(outliers.index, axis=0, inplace=True)
-data_outliers_rowDrop.to_csv("../data/CovidPos_outliers_rowDrop_stdBased.csv", index=False)
+data_outliers_rowDrop.to_csv("../data/CovidPos_outliers_rowDrop_stdBased.csv")
 print(f"Data after dropping outliers: {data_outliers_rowDrop.shape}")
+outliers_evaluation("../data/CovidPos_outliers_rowDrop_stdBased.csv", "rowDrop_stdBased")
 
 
 # Approach 2 - Dropping outliers (std_based = false)
@@ -26,39 +45,29 @@ for var in data.columns:
     top_threshold, bottom_threshold = determine_outlier_thresholds_for_var(summary5[var], std_based=False)
     outliers: Series = data_outliers_rowDrop[(data_outliers_rowDrop[var] > top_threshold) | (data_outliers_rowDrop[var] < bottom_threshold)]
     data_outliers_rowDrop.drop(outliers.index, axis=0, inplace=True)
-data_outliers_rowDrop.to_csv("../data/CovidPos_outliers_rowDrop_NotStdBased.csv", index=False)
+data_outliers_rowDrop.to_csv("../data/CovidPos_outliers_rowDrop_NotStdBased.csv")
 print(f"Data after dropping outliers: {data_outliers_rowDrop.shape}")
+outliers_evaluation("../data/CovidPos_outliers_rowDrop_NotStdBased.csv", "rowDrop_NotStdBased")
 
 
 # Approach 3 - Replacing Outliers (if > than max or < min) with fixed values - using the median
+print(f"Data before replacing outliers: {data.shape}")
 data_outliers_rep_fixedMedian: DataFrame = data.copy(deep=True)
 for var in data.columns:
     top, bottom = determine_outlier_thresholds_for_var(summary5[var])
     median: float = data_outliers_rep_fixedMedian[var].median()
     data_outliers_rep_fixedMedian[var] = data_outliers_rep_fixedMedian[var].apply(lambda x: median if x > top or x < bottom else x)
-data_outliers_rep_fixedMedian.to_csv("../data/CovidPos_outliers_rep_fixed_median.csv", index=True)
+data_outliers_rep_fixedMedian.to_csv("../data/CovidPos_outliers_rep_fixed_median.csv")
+print(f"Data after replacing outliers: {data_outliers_rep_fixedMedian.shape}")
+outliers_evaluation("../data/CovidPos_outliers_rep_fixed_median.csv", "rep_fixed_median")
+
 
 # Approach 4 - Truncating outliers with the max and min
+print(f"Data before truncating outliers: {data.shape}")
 data_outliers_trunc: DataFrame = data.copy(deep=True)
 for var in data.columns:
     top, bottom = determine_outlier_thresholds_for_var(summary5[var])
     data_outliers_trunc[var] = data_outliers_trunc[var].apply(lambda x: top if x > top else bottom if x < bottom else x)
-data_outliers_trunc.to_csv("../data/CovidPos_outliers_trunc_minmax.csv", index=True)
-
-
-
-def outliers_evaluation(data_filename: str, strategy: str):
-    data: DataFrame = read_csv(data_filename)
-    figure()
-    eval: dict[str, list] = evaluate_approach(data.head(int(data.shape[0]*0.7)),
-                                              data.tail(int(data.shape[0]*0.3)),
-                                              target=target, metric="recall")
-    plot_multibar_chart(["NB", "KNN"], eval, title=f"Outliers Treatment with {strategy} evaluation", percentage=True)
-    savefig(f"../images/CovidPos_outliers_treat_{strategy}.png")
-    show()
-    
-# Evaluate the different approaches
-outliers_evaluation("../data/CovidPos_outliers_rowDrop_stdBased.csv", "rowDrop_stdBased")
-outliers_evaluation("../data/CovidPos_outliers_rowDrop_NotStdBased.csv", "rowDrop_NotStdBased")
-outliers_evaluation("../data/CovidPos_outliers_rep_fixed_median.csv", "rep_fixed_median")
+data_outliers_trunc.to_csv("../data/CovidPos_outliers_trunc_minmax.csv")
+print(f"Data after truncating outliers: {data_outliers_trunc.shape}")
 outliers_evaluation("../data/CovidPos_outliers_trunc_minmax.csv", "truncating_minmax")
--- dataset_1/2-data_preparation/code/scaling.py
+++ dataset_1/2-data_preparation/code/scaling.py
@@ -1,43 +1,63 @@
 from pandas import read_csv, DataFrame, Series
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from matplotlib.pyplot import figure, savefig, close, show, subplots
+from dslabs_functions import  plot_multibar_chart, evaluate_approach
+from sklearn.model_selection import train_test_split
 
-data_filename: str = "../data/CovidPos_outliers_rowDrop_NotStdBased.csv"
+
+def scaling_evaluation(data_filename: str, strategy: str, target: str):
+    data: DataFrame = read_csv(data_filename)
+
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+    ## Evaluate Approach
+    file_tag = "CovidPos"
+    target = "CovidPos"
+
+    figure()
+    eval: dict[str, list] = evaluate_approach(train_data, test_data, target=target, metric="recall")
+    plot_multibar_chart(
+        ["NB", "KNN"], eval, title=f"{file_tag} evaluation", percentage=True)
+
+    savefig(f"../images/CovidPos_scaling_treat_{strategy}.png")
+    show()
+
+data_filename: str = "../data/CovidPos_outliers_trunc_minmax.csv"
 file_tag: str = "CovidPos"
 target = "CovidPos"
 
 data: DataFrame = read_csv(data_filename)
+#scaling_evaluation(data_filename, "Original", target)
 
 # Approach 1 - Standard scaling (z-score transformation)
 vars: list[str] = data.columns.to_list()
-target_data: Series = data.pop(file_tag)
+vars.remove(target)
+target_data: Series = data.pop(target)
 
-transf: StandardScaler = StandardScaler(with_mean=True, with_std=True, copy=True).fit(
-    data
-)
+transf: StandardScaler = StandardScaler(with_mean=True, with_std=True, copy=True).fit(data)
 df_zscore = DataFrame(transf.transform(data), index=data.index)
-df_zscore[file_tag] = target_data
 df_zscore.columns = vars
-df_zscore.to_csv(f"../data/{file_tag}_scaled_zscore.csv", index="ID")
+df_zscore[target] = target_data
+df_zscore.to_csv(f"../data/CovidPos_scaled_zscore.csv", index=False)
 print(f"Data after standard scaling: {df_zscore.head(20)}")
+#print(get_variable_types(df_zscore))
+scaling_evaluation("../data/CovidPos_scaled_zscore.csv", "Z-Score", target)
 
 # Approach 2 - Minmax Scaler [0,1]
 transf: MinMaxScaler = MinMaxScaler(feature_range=(0, 1), copy=True).fit(data)
 df_minmax = DataFrame(transf.transform(data), index=data.index)
 df_minmax[target] = target_data
 df_minmax.columns = vars
-df_minmax.to_csv(f"../data/{file_tag}_scaled_minmax.csv", index="id")
+df_minmax.to_csv(f"../data/CovidPos_scaled_minmax.csv", index=False)
 print(f"Data after minmax scaling: {df_minmax.head(20)}")
+#print(get_variable_types(df_minmax))
 
-# Evaluate the different approaches
-fig, axs = subplots(1, 3, figsize=(25, 10), squeeze=False)
-axs[0, 0].set_title("Original data")
-data.boxplot(ax=axs[0, 0])
-axs[0, 1].set_title("Z-score normalization")
-df_zscore.boxplot(ax=axs[0, 1])
-# axs[0, 2].set_title("MinMax2 normalization")
-# df_minmaxRange.boxplot(ax=axs[0, 2])
-axs[0, 2].set_title("MinMax normalization")
-df_minmax.boxplot(ax=axs[0, 2])
-savefig(f"../images/CovidPos_scaling.png")
-show()
+#Approach 2 - Minmax Scaler2 [0,10]
+transf: MinMaxScaler = MinMaxScaler(feature_range=(-1, 1), copy=True).fit(data)
+df_minmaxRange = DataFrame(transf.transform(data), index=data.index)
+df_minmaxRange[target] = target_data
+df_minmaxRange.columns = vars
+df_minmaxRange.to_csv(f"../data/CovidPos_scaled_minmaxRange.csv", index="id")
+print(f"Data after minmax scaling: {df_minmaxRange.head(20)}")
+
+scaling_evaluation("../data/CovidPos_scaled_minmaxRange.csv", "MinMax", target)
+
