98e1a82e5eb9e0e2f4cce5fe4043cd47b2a50640
Matilde Heitor <matildenheitor@tecnico.ulisboa.pt>
2023-12-07 11:45:32 +0000

    rf

--- dataset2/dslabs_functions.py
+++ dataset2/dslabs_functions.py
@@ -29,6 +29,7 @@ from sklearn.metrics import confusion_matrix, RocCurveDisplay, roc_auc_score
 from sklearn.naive_bayes import _BaseNB, GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 from config import ACTIVE_COLORS, LINE_COLOR, FILL_COLOR, cmap_blues
 from typing import Literal
@@ -854,7 +855,60 @@ def mlp_study(
     )
 
     return best_model, best_params
-    
+   
+def random_forests_study(
+    trnX: ndarray,
+    trnY: array,
+    tstX: ndarray,
+    tstY: array,
+    nr_max_trees: int = 2500,
+    lag: int = 500,
+    metric: str = "accuracy",
+) -> tuple[RandomForestClassifier | None, dict]:
+    n_estimators: list[int] = [100] + [i for i in range(500, nr_max_trees + 1, lag)]
+    max_depths: list[int] = [2, 5, 7]
+    max_features: list[float] = [0.1, 0.3, 0.5, 0.7, 0.9]
+
+    best_model: RandomForestClassifier | None = None
+    best_params: dict = {"name": "RF", "metric": metric, "params": ()}
+    best_performance: float = 0.0
+
+    values: dict = {}
+
+    cols: int = len(max_depths)
+    _, axs = subplots(1, cols, figsize=(cols * HEIGHT, HEIGHT), squeeze=False)
+    for i in range(len(max_depths)):
+        d: int = max_depths[i]
+        values = {}
+        for f in max_features:
+            y_tst_values: list[float] = []
+            for n in n_estimators:
+                clf = RandomForestClassifier(
+                    n_estimators=n, max_depth=d, max_features=f
+                )
+                clf.fit(trnX, trnY)
+                prdY: array = clf.predict(tstX)
+                eval: float = CLASS_EVAL_METRICS[metric](tstY, prdY)
+                y_tst_values.append(eval)
+                if eval - best_performance > DELTA_IMPROVE:
+                    best_performance = eval
+                    best_params["params"] = (d, f, n)
+                    best_model = clf
+                # print(f'RF d={d} f={f} n={n}')
+            values[f] = y_tst_values
+        plot_multiline_chart(
+            n_estimators,
+            values,
+            ax=axs[0, i],
+            title=f"Random Forests with max_depth={d}",
+            xlabel="nr estimators",
+            ylabel=metric,
+            percentage=True,
+        )
+    print(
+        f'RF best for {best_params["params"][2]} trees (d={best_params["params"][0]} and f={best_params["params"][1]})'
+    )
+    return best_model, best_params
 
 def evaluate_approach(
     train: DataFrame, test: DataFrame, target: str = "class", metric: str = "accuracy"
--- dataset2/lab4.py
+++ dataset2/lab4.py
@@ -1,14 +1,99 @@
 from pandas import read_csv, DataFrame
 from dslabs_functions import (read_train_test_from_files, mlp_study, HEIGHT, plot_evaluation_results,
-                              plot_multiline_chart, CLASS_EVAL_METRICS)
+                              plot_multiline_chart, CLASS_EVAL_METRICS, plot_horizontal_bar_chart,
+                              random_forests_study)
 from matplotlib.pyplot import show, savefig, figure, subplots
-from numpy import array, ndarray, argsort
+from numpy import array, ndarray, argsort, std
 from typing import Literal
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from subprocess import call
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 
+#***************************************************************************************************
+#                                          Random Forests                                               *
+#***************************************************************************************************
 
+def rf(trnX: DataFrame, trnY:DataFrame, tstX: DataFrame, tstY:DataFrame, eval_metric: str):
+    figure()
+    print("starting rf")
+    best_model, params = random_forests_study(
+        trnX,
+        trnY,
+        tstX,
+        tstY,
+        nr_max_trees=1000,
+        lag=250,
+        metric=eval_metric,
+    )
+    print("got best model")
+    savefig(f"images/{file_tag}_rf_{eval_metric}_study.png")
+
+    if eval_metric == "accuracy":
+        print("overfitting")
+        rf_performance(trnX, trnY, tstX, tstY, best_model, params, labels)
+        rf_feature_importance(best_model, eval_metric)
+        rf_overfitting(trnX, trnY, tstX, tstY, params, eval_metric)
+
+def rf_performance(trnX: DataFrame, trnY:DataFrame, tstX: DataFrame, tstY:DataFrame, best_model, params, labels):
+    prd_trn: array = best_model.predict(trnX)
+    prd_tst: array = best_model.predict(tstX)
+    figure()
+    plot_evaluation_results(params, trnY, prd_trn, tstY, prd_tst, labels)
+    savefig(f'images/{file_tag}_rf_{params["name"]}_best_{params["metric"]}_eval.png')
+
+def rf_feature_importance(best_model, eval_metric = "accuracy"):
+    stdevs: list[float] = list(
+        std([tree.feature_importances_ for tree in best_model.estimators_], axis=0)
+    )
+    importances = best_model.feature_importances_
+    indices: list[int] = argsort(importances)[::-1]
+    elems: list[str] = []
+    imp_values: list[float] = []
+    for f in range(len(vars)):
+        elems += [vars[indices[f]]]
+        imp_values.append(importances[indices[f]])
+        print(f"{f+1}. {elems[f]} ({importances[indices[f]]})")
+
+    figure()
+    plot_horizontal_bar_chart(
+        elems,
+        imp_values,
+        error=stdevs,
+        title="RF variables importance",
+        xlabel="importance",
+        ylabel="variables",
+        percentage=True,
+    )
+    savefig(f"images/{file_tag}_rf_{eval_metric}_vars_ranking.png")
+
+def rf_overfitting(trnX: DataFrame, trnY:DataFrame, tstX: DataFrame, tstY:DataFrame, params, eval_metric = "accuracy"):
+    d_max: int = params["params"][0]
+    feat: float = params["params"][1]
+    nr_estimators: list[int] = [i for i in range(2, 2501, 500)]
+
+    y_tst_values: list[float] = []
+    y_trn_values: list[float] = []
+    acc_metric: str = "accuracy"
+
+    for n in nr_estimators:
+        clf = RandomForestClassifier(n_estimators=n, max_depth=d_max, max_features=feat)
+        clf.fit(trnX, trnY)
+        prd_tst_Y: array = clf.predict(tstX)
+        prd_trn_Y: array = clf.predict(trnX)
+        y_tst_values.append(CLASS_EVAL_METRICS[acc_metric](tstY, prd_tst_Y))
+        y_trn_values.append(CLASS_EVAL_METRICS[acc_metric](trnY, prd_trn_Y))
+
+    figure()
+    plot_multiline_chart(
+        nr_estimators,
+        {"Train": y_trn_values, "Test": y_tst_values},
+        title=f"RF overfitting study for d={d_max} and f={feat}",
+        xlabel="nr_estimators",
+        ylabel=str(eval_metric),
+        percentage=True,
+    )
+    savefig(f"images/{file_tag}_rf_{eval_metric}_overfitting.png")
 #***************************************************************************************************
 #                                             MLP                                                  *
 #***************************************************************************************************
@@ -107,7 +192,8 @@ if __name__ == "__main__":
     fig.suptitle("Decision trees study for different parameters")
     i, j = 0, 0
     for metric in eval_metrics:
-        mlp(trnX, trnY, tstX, tstY, metric)
+        # mlp(trnX, trnY, tstX, tstY, metric)
+        rf(trnX, trnY, tstX, tstY, metric)
   
     
     #DT_best_model_performance(trnX, trnY, tstX, tstY, depth=14)
