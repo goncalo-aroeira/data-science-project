268a0996476ba37de4cf8a741ec010e1ce73b221
Jorge Santos <jorge.m.santos@tecnico.ulisboa.pt>
2023-12-26 16:31:58 +0000

    updated images for data profiling

--- dataset_1/1-data_profiling/code/4.2-correlation.py
+++ dataset_1/1-data_profiling/code/4.2-correlation.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 from pandas import DataFrame
-from dataset_1.dslabs_functions import get_variable_types
+from dslabs_functions import get_variable_types
 from pandas import read_csv
 
-filename = "../../class_pos_covid.csv"
+filename = "../../2-data_preparation/data/ccs_vars_encoded.csv"
 file_tag = "CovidPos"
 data: DataFrame = read_csv(filename)
 
 variables_types: dict[str, list] = get_variable_types(data)
 numeric: list[str] = variables_types["numeric"]
-corr_mtx: DataFrame = data[numeric].corr().abs()
+binary: list[str] = variables_types["binary"]
+corr_mtx: DataFrame = data[numeric+binary].corr().abs()
 
 # Create a heatmap using matplotlib
 fig, ax = plt.subplots(figsize=(10, 8))
 cax = ax.matshow(abs(corr_mtx), cmap='Blues')
-plt.xticks(range(len(numeric)), numeric, rotation=45, ha='left')
-plt.yticks(range(len(numeric)), numeric)
+plt.xticks(range(len(numeric)+len(binary)), numeric+binary, rotation=45, ha='left')
+plt.yticks(range(len(numeric)+len(binary)), numeric+binary)
 plt.colorbar(cax)
 
 plt.savefig(f"../images/{file_tag}_correlation_analysis.png")
