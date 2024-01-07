890796e46af62917fb8018d7c8a3b0f7ac32c7f9
Jorge Miguel Oliveira Santos <jorge.m.santos@tecnico.ulisboa.pt>
2024-01-07 18:20:39 +0000

    Fixed some errors; updated images for granularity for d1

--- dataset_1/2-data_preparation/code/variables.py
+++ dataset_1/2-data_preparation/code/variables.py
@@ -15,56 +15,56 @@ contagem_por_regiao = data[column_to_plot].value_counts()
 file_tag = "CovidPos_State_per_region"
 
 mapeamento_regioes = {
-    'Alabama': 'Sul',
-    'Alaska': 'Oeste',
-    'Arizona': 'Oeste',
-    'Arkansas': 'Sul',
-    'Califórnia': 'Oeste',
-    'Colorado': 'Oeste',
-    'Connecticut': 'Nordeste',
-    'Delaware': 'Nordeste',
-    'Flórida': 'Sul',
-    'Geórgia': 'Sul',
-    'Havaí': 'Oeste',
-    'Idaho': 'Oeste',
-    'Illinois': 'Meio-Oeste',
-    'Indiana': 'Meio-Oeste',
-    'Iowa': 'Meio-Oeste',
-    'Kansas': 'Meio-Oeste',
-    'Kentucky': 'Sul',
-    'Louisiana': 'Sul',
-    'Maine': 'Nordeste',
-    'Maryland': 'Nordeste',
-    'Massachusetts': 'Nordeste',
-    'Michigan': 'Meio-Oeste',
-    'Minnesota': 'Meio-Oeste',
-    'Mississippi': 'Sul',
-    'Missouri': 'Meio-Oeste',
-    'Montana': 'Oeste',
-    'Nebraska': 'Meio-Oeste',
-    'Nevada': 'Oeste',
-    'New Hampshire': 'Nordeste',
-    'New Jersey': 'Nordeste',
-    'Novo México': 'Oeste',
-    'Nova York': 'Nordeste',
-    'Carolina do Norte': 'Sul',
-    'Dakota do Norte': 'Meio-Oeste',
-    'Ohio': 'Meio-Oeste',
-    'Oklahoma': 'Sul',
-    'Oregon': 'Oeste',
-    'Pensilvânia': 'Nordeste',
-    'Rhode Island': 'Nordeste',
-    'Carolina do Sul': 'Sul',
-    'Dakota do Sul': 'Meio-Oeste',
-    'Tennessee': 'Sul',
-    'Texas': 'Sul',
-    'Utah': 'Oeste',
-    'Vermont': 'Nordeste',
-    'Virgínia': 'Sul',
-    'Washington': 'Oeste',
-    'Virgínia Ocidental': 'Sul',
-    'Wisconsin': 'Meio-Oeste',
-    'Wyoming': 'Oeste'
+    'Alabama': 'South',
+    'Alaska': 'West',
+    'Arizona': 'West',
+    'Arkansas': 'South',
+    'Califórnia': 'West',
+    'Colorado': 'West',
+    'Connecticut': 'NorthEast',
+    'Delaware': 'NorthEast',
+    'Flórida': 'South',
+    'Geórgia': 'South',
+    'Havaí': 'West',
+    'Idaho': 'West',
+    'Illinois': 'MidWest',
+    'Indiana': 'MidWest',
+    'Iowa': 'MidWest',
+    'Kansas': 'MidWest',
+    'Kentucky': 'South',
+    'Louisiana': 'South',
+    'Maine': 'NorthEast',
+    'Maryland': 'NorthEast',
+    'Massachusetts': 'NorthEast',
+    'Michigan': 'MidWest',
+    'Minnesota': 'MidWest',
+    'Mississippi': 'South',
+    'Missouri': 'MidWest',
+    'Montana': 'West',
+    'Nebraska': 'MidWest',
+    'Nevada': 'West',
+    'New Hampshire': 'NorthEast',
+    'New Jersey': 'NorthEast',
+    'Novo México': 'West',
+    'Nova York': 'NorthEast',
+    'Carolina do Norte': 'South',
+    'Dakota do Norte': 'MidWest',
+    'Ohio': 'MidWest',
+    'Oklahoma': 'South',
+    'Oregon': 'West',
+    'Pensilvânia': 'NorthEast',
+    'Rhode Island': 'NorthEast',
+    'Carolina do South': 'South',
+    'Dakota do South': 'MidWest',
+    'Tennessee': 'South',
+    'Texas': 'South',
+    'Utah': 'West',
+    'Vermont': 'NorthEast',
+    'Virgínia': 'South',
+    'Washington': 'West',
+    'Virgínia Ocidental': 'South',
+    'Wisconsin': 'MidWest',
+    'Wyoming': 'West'
 }
 
 # Adicionar uma nova coluna 'regiao' ao DataFrame com base no mapeamento
@@ -75,9 +75,9 @@ contagem_por_regiao = data['regiao'].value_counts()
 
 # Criar o histograma
 contagem_por_regiao.plot(kind='bar', color='green')
-plt.xlabel('Regiões Geográficas')
-plt.ylabel('Número de Registros')
-plt.title('Número de Registros por Região')
+plt.xlabel('Geographical Regions')
+plt.ylabel('Number of Records')
+plt.title('Number of Records per Region')
 plt.savefig(f"{file_tag}_study_for_granularity.png", bbox_inches='tight')
 plt.show()
 
@@ -87,7 +87,7 @@ value_counts = data[column_to_plot].value_counts()
 
 # Chamar a função de plotagem de gráfico de barras
 plt.figure(figsize=(20, 6))
-plot_bar_chart(value_counts.index, value_counts.values, title='Granularity Study for variable LastCheckupTime', xlabel='Value', ylabel='Number of records')
+plot_bar_chart(value_counts.index, value_counts.values, title='Granularity Study for variable State', xlabel='State', ylabel='Number of records')
 plt.xticks(rotation=45, ha='right')  # Ajuste de rotação
 plt.savefig(f"{file_tag}_study_for_granularity.png", bbox_inches='tight')
 
